8c084d6fd27304c9777569b3b209be8bf8a10f45
Johan Katherine Norena Bahamon <jknorenab@darkstar.org>
2019-09-06 11:12:41 -0500

    Hola!!
    Introducimos el trabajo en clase kjkncrybfabkjf

---
+++ 2019-09-04-Intro/holamundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main(void)
+{
+  float x= 0.0; // precision sencilla
+  double z= 2.0e-6; // Precision doble
+  
+  std:: cout<<"Hola mundo"<< std:: endl;
+
+  std:: cout<< z << "\n";
+    
+  return 0;
+
+}
