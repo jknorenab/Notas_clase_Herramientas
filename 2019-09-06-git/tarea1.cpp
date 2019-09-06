b4c9a96a904b4e9bf9db704209b08a69d013b4da
Johan Katherine Norena Bahamon <jknorenab@darkstar.org>
2019-09-06 12:53:04 -0500

    tarea1

---
+++ 2019-09-06-git/tarea1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main (void){
+  int prueba;
+  prueba = 0;
+  while(prueba>=0){
+  prueba +=10;
+  }
+  return 0;
+}
