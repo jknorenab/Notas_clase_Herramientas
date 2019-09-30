de26498fb4879691a104baf20b144cbb04b1f137
Johan Katherine Norena Bahamon <jknorenab@darkstar.org>
2019-09-30 16:23:13 -0500

    Corregimos el programa hecho en clase

--- 2019-09-11-errors/seno-error.cpp
+++ 2019-09-11-errors/seno-error.cpp
@@ -2,23 +2,25 @@
 #include <fstream> //para imprimir a archivos
 #include <cmath>
 #include <cstdlib>
+#include <iostream>
 
 
 int main(void){
   
-  std:: ofstream fout("datos.txt");
+  std:: ofstream fout("datos.txt"); //exporta el archivo binario a un archivo de texto con la tabla de datos que queremos!!!
   
-  double x = 0.23;
+  //double x = 0.0123; // radianes
   const int N = 100; //iteraciones de la suma
 
-  for(x = 0.1; x < 8.0; x += 0.1){
-  double sum = x; //variable donde guardo la suma
-  double term = x; 
-  for (int ii = 2; ii < N; ++ii){
-    term = term*(-1)*x*x/((2*ii)*(2*ii+1));
-    sum += term;
-  }
-  fout << x << "\t" << term << "t" << "t" << std::sin(x) << "\n";
+  for(double x = 0.1; x < 8.0; x += 0.1){ // en este primer for calculamos sen(x) para varios x, es decir 0.1 < x < 8.0
+    double sum = x; //variable donde guardo la suma
+    double term = x; // aquí guardamos los terminos de la suma
+    for (int ii = 1; ii < N; ++ii){ // este for calcula el seno para el x dado por el anterior for
+      //std::cout << term << "\n";
+      term = term*(-1)*x*x/((2*ii)*(2*ii+1));
+      sum = sum + term;
+    }
+    fout << x << "\t " << sum  << "\t" << "\t" << std::sin(x) << "\n";
   }
   fout.close();
   return 0;
