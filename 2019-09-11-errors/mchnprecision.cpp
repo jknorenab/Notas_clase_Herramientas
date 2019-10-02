ce8fc74cd8e6fb8ca2fc3fa7d2fb41a4922f8390
Johan Katherine Norena Bahamon <jknorenab@unal.edu.co>
2019-10-02 16:32:43 -0500

    Modificamosel comentario

--- 2019-09-11-errors/mchnprecision.cpp
+++ 2019-09-11-errors/mchnprecision.cpp
@@ -1,7 +1,7 @@
 #include <cstdio> // para imprimir con printf
 #include <cmath>
 #include <cstdlib>
-// En este programa le pedimos al computador, con un simple código, que nos muestre hasta qué punto es capaz de diferenciar un número pequeño de otro. Así, le decimos al programa que le sume una variable (eps) que se hace cada vez más pequeña; a una constante (one)  por cada paso del loop (1000). Le pedimos, entonces que imprima el paso, la suma y la variable pequeña. El resultado es un underflow en e-45 (?) que es cuando el computador deja  de reconocer la variable eps y la hace cero.
+// En este programa le pedimos al computador, con un simple código, que nos muestre hasta qué punto es capaz de diferenciar un número pequeño de otro. Así, le decimos al programa que le sume una variable (eps) que se hace cada vez más pequeña; a una constante (one)  por cada paso del loop (1000). Le pedimos, entonces que imprima el paso, la suma y la variable pequeña. El resultado es un underflow en e-45 que es cuando el computador deja  de reconocer la variable eps y la hace cero.
 
 int main(void){
 
